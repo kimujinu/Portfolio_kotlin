5c7c7ddc0e5d6536f4c05da73b596ac373770654
Jin-Woo Kim <dnehd6739@naver.com>
2021-01-02 20:21:16 +0900

    contents Data Model

--- app/src/main/java/navigation/AddPhotoActivity.kt
+++ app/src/main/java/navigation/AddPhotoActivity.kt
@@ -6,9 +6,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import bolts.Task
 import com.example.portfolio_kotlin.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_add_photo.*
+import navigation.model.ContentDTO
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -16,11 +21,15 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    var auth : FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
@@ -49,9 +58,51 @@ class AddPhotoActivity : AppCompatActivity() {
         var imageFileName = "IMAGE_" + timestamp + "_.png"
 
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+        //Promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task : com.google.android.gms.tasks.Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
 
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+            contentDTO.imageURI = uri.toString()
+
+            contentDTO.uid = auth?.currentUser?.uid
+
+            contentDTO.userid = auth?.currentUser?.email
+
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
         }
+        //Callback method
+//        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+//            //Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+//            storageRef.downloadUrl.addOnSuccessListener {uri ->
+//                var contentDTO = ContentDTO()
+//
+//                contentDTO.imageURI = uri.toString()
+//
+//                contentDTO.uid = auth?.currentUser?.uid
+//
+//                contentDTO.userid = auth?.currentUser?.email
+//
+//                contentDTO.explain = addphoto_edit_explain.text.toString()
+//
+//                contentDTO.timestamp = System.currentTimeMillis()
+//
+//                firestore?.collection("images")?.document()?.set(contentDTO)
+//
+//                setResult(Activity.RESULT_OK)
+//
+//                finish()
+//
+//            }
+//        }
     }
 }
---
+++ app/src/main/java/navigation/model/ContentDTO.kt
@@ -0,0 +1,14 @@
+package navigation.model
+
+data class ContentDTO(var explain : String? = null,
+                      var imageURI : String? = null,
+                      var uid : String? = null,
+                      var userid : String? = null,
+                      var timestamp : Long? = null,
+                      var favoriteCount : Int = 0,
+                      var favorites : Map<String,Boolean> = HashMap()) {
+    data class Comment(var uid : String? = null,
+                       var userid : String? = null,
+                       var comment : String? = null,
+                       var timestamp : Long? = null)
+}
